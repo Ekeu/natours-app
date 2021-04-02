929da32251116ae2383b17a033012a119c26aedb
Ekeu Ulrich <ekeuulrich2@gmail.com>
2021-04-02 23:07:36 +0200

    Import dev-data script to database

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,51 @@
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const fs = require('fs');
+const Tour = require('../../models/tourModel');
+
+dotenv.config();
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect(DB, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((conn) => {
+    console.log(`MongoDB Connected : ${conn.connection.host}`);
+  });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+);
+
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data successfully loaded...');
+  } catch (error) {
+    console.log(error);
+  }
+  process.exit();
+};
+
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('All data successfully deleted...');
+  } catch (error) {
+    console.log(error);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
